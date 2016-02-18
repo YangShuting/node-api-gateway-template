4a2a5f550e3430b01cd0b7dd1ad0705d1cf85f5b
simonedesordi <simone.desordi@gmail.com>
2016-02-18 15:25:42 +0100

    securing api, added log level

--- src/app.js
+++ src/app.js
@@ -1,35 +1,75 @@
 require("babel-core/register");
 require("babel-polyfill");
 
+// app arguments
 import minimist from 'minimist';
-import winston from 'winston';
-const log = winston;
-log.level = 'debug';
-
 const args = minimist(process.argv.slice(2));
 
-var express = require('express');
-var app = express();
-var falcorExpress = require('falcor-express');
-var Router = require('falcor-router');
-import jwt from 'express-jwt'
+// logger
+import winston from 'winston';
+const logger = winston;
+logger.info('set log level to ' + args.logLevel);
+logger.level = args.logLevel || 'info'; // set log level
+logger.info('setted log level to ' + logger.level);
+
+import express from 'express';
+const app = express();
+import * as falcorExpress from 'falcor-express';
+import Router from 'falcor-router';
+import jwt from 'express-jwt';
+import config from './config.json';
 
 var debug = require('debug')('app:' + process.pid),
     path = require("path"),
-    fs = require("fs"),
-    config = require("./config.json"),
     onFinished = require('on-finished'),
     NotFoundError = require(path.join(__dirname, "errors", "NotFoundError.js")),
     utils = require(path.join(__dirname, "utils.js")),
     unless = require('express-unless');
 
-debug("Attaching plugins");
 app.use(require('morgan')("dev")); // log server
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(require('compression')());
 app.use(require('response-time')());
+
+var port = function () {
+    var portGlobal = process.env.PORT;
+    logger.info("portGlobal = " + portGlobal);
+
+    var port = portGlobal /*|| portParameter*/ || '9090';
+    logger.info("port = " + port);
+
+    return port;
+};
+
+const publicDataSourceRouter = function (req, res) {
+    // create a Virtual JSON resource with single key ("greeting")
+    return new Router([
+        {
+            // match a request for the key "greeting"
+            route: "greeting",
+            // respond with a PathValue with the value of "Hello World."
+            get: function () {
+                return {path: ["greeting"], value: "Hello world"};
+            }
+        }
+    ]);
+};
+
+const privateDataSourceRouter = function (req, res) {
+    // create a Virtual JSON resource with single key ("greeting")
+    return new Router([
+        {
+            // match a request for the key "greeting"
+            route: "login",
+            // respond with a PathValue with the value of "Hello World."
+            get: function () {
+                return {path: ["login"], value: "Private Hello World"};
+            }
+        }
+    ]);
+};
 
 app.use(function (req, res, next) {
 
@@ -73,55 +113,18 @@ const publicPath = {
 
  */
 
+app.use('/api/public/model.json', falcorExpress.dataSourceRoute(publicDataSourceRouter));
+app.use('/api/private/model.json', jwtCheck, falcorExpress.dataSourceRoute(privateDataSourceRouter));
+
 //TODO
-//app.use("/api/private", jwtCheck.unless(publicPath));
-//app.use("/api/private", utils.middleware().unless(publicPath));
+app.use("/api/private", jwtCheck.unless(publicPath));
+app.use("/api/private", utils.middleware().unless(publicPath));
 app.use("/api", require(path.join(__dirname, "routes", "default.js"))());
 
 // https://matoski.com/article/jwt-express-node-mongoose/#jwt
 // http://netflix.github.io/falcor/documentation/router.html
 // https://github.com/auth0/express-jwt
 
-var port = function () {
-    var portGlobal = process.env.PORT;
-    console.log("portGlobal = " + portGlobal);
-
-    var port = portGlobal /*|| portParameter*/ || '9090';
-    console.log("port = " + port);
-
-    return port;
-};
-
-const publicDataSourceRouter = function (req, res) {
-    // create a Virtual JSON resource with single key ("greeting")
-    return new Router([
-        {
-            // match a request for the key "greeting"
-            route: "greeting",
-            // respond with a PathValue with the value of "Hello World."
-            get: function () {
-                return {path: ["greeting"], value: "Hello world"};
-            }
-        }
-    ]);
-};
-
-const privateDataSourceRouter = function (req, res) {
-    // create a Virtual JSON resource with single key ("greeting")
-    return new Router([
-        {
-            // match a request for the key "greeting"
-            route: "login",
-            // respond with a PathValue with the value of "Hello World."
-            get: function () {
-                return {path: ["login"], value: "Private Hello World"};
-            }
-        }
-    ]);
-};
-
-app.use('/api/public/model.json', falcorExpress.dataSourceRoute(publicDataSourceRouter));
-app.use('/api/private/model.json', falcorExpress.dataSourceRoute(privateDataSourceRouter));
 // serve static files from current directory
 app.use(express.static(__dirname + '/'));
 
@@ -135,7 +138,7 @@ app.use(function (err, req, res, next) {
 
     var errorType = typeof err,
         code = 500,
-        msg = { message: "Internal Server Error" };
+        msg = {message: "Internal Server Error"};
 
     switch (err.name) {
         case "UnauthorizedError":
